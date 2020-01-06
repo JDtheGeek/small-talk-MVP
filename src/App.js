a814a531ddd288239d2e3a953479fe9b1eab258a
JDtheGeek <john.dennehy@gmail.com>
2020-01-06 12:55:07 +0000

    adds basic structure and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Small Talk</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Small Talk</h1>
+      </div>
+    </Router>
   );
 }
 
