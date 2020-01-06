7c7993dc9d7f3590e5e471c6f7d16aabf57f1c65
JDtheGeek <john.dennehy@gmail.com>
2020-01-06 13:31:33 +0000

    adds Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Small Talk</h1>
+        <Navbar />
       </div>
     </Router>
   );
