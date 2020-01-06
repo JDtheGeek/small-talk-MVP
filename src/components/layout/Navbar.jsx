6bf33efcd07565afc9118b6afa89c5e4bcac1ca2
JDtheGeek <john.dennehy@gmail.com>
2020-01-06 13:44:32 +0000

    adds links to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -7,6 +8,13 @@ const Navbar = () => {
       <div className="container">
         <Link to='/' className='brand-logo'>Small Talk</Link>
       </div>
+      <ul className='right'>
+        <li><NavLink to='/'>Sign Up</NavLink></li>
+        <li><NavLink to='/'>Sign In</NavLink></li>
+        <li><NavLink to='/'>New Article</NavLink></li>
+        <li><NavLink to='/'>Log Out</NavLink></li>
+        <li><NavLink to='/' className='btn btn-floating green lighten-1'>NCT</NavLink></li>
+      </ul>
     </nav>
 
   )
